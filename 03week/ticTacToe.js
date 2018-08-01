82bc5b50b3087ed15cb85b75a88f69ca472d088a
maryhepp <heppenstall.mary@gmail.com>
2018-08-01 17:49:08 -0500

    Tic Tac Toe

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -23,24 +23,79 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
+// Whiteboarding 
+// Player needs to be swithched after each turn
+// Check for player turn's horitzontal win
+// Check for player turn's vertical win
+// Check for player turn's diagonal win
+// If win found, output 'You Win!'
+// If winner found, reset board
+
+
+// Function
+
 function horizontalWin() {
-  // Your code here
+  if([0][0] === playerTurn && [0][1] === playerTurn && [0][2] === playerTurn){
+    return true;
+  }
+  if([1][0] === playerTurn && [1][1] === playerTurn && [1][2] === playerTurn){
+    return true;
+  }
+  if([2][0] === playerTurn && [2][1] === playerTurn && [2][2] === playerTurn){
+    return true;
+  }
 }
 
 function verticalWin() {
-  // Your code here
+  if([0][0] === playerTurn && [1][0] === playerTurn && [2][0] === playerTurn){
+    return true;
+  }
+  if([0][1] === playerTurn && [1][1] === playerTurn && [2][1] === playerTurn){
+    return true;
+  }
+  if([0][2] === playerTurn && [1][2] === playerTurn && [2][2] === playerTurn){
+    return true;
+  }
 }
 
 function diagonalWin() {
-  // Your code here
+  if([0][0] === playerTurn && [1][1] === playerTurn && [2][2] === playerTurn){
+    return true;
+  }
+  if([0][2] === playerTurn && [1][1] === playerTurn && [2][0] === playerTurn){
+    return true;
+  }
 }
 
 function checkForWin() {
-  // Your code here
+  if (horizontalWin()) {
+    console.log('horizontal win');
+    return "You Win!"
+  }
+  if (verticalWin()){
+    console.log('vertical win');
+    return "You Win!"
+  }
+  if (diagonalWin()){
+    console.log('diagonal win');
+    return "You Win!"
+  }
 }
 
+// check for win
 function ticTacToe(row, column) {
-  // Your code here
+  if (board[row][column] === ' ') {
+    board[row][column] = playerTurn;
+  if (checkForWin()){ 
+    board = [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
+    ];
+  } else {
+    return playerTurn;
+    }
+  }
 }
 
 function getPrompt() {
