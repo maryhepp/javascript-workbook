3fc099124a05898d910ecd7586119b472a1d1ba5
Mary Heppenstall <heppenstall.mary@gmail.com>
2018-08-27 20:40:31 -0500

    Update higherOrder.js

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -3,23 +3,39 @@
 const assert = require('assert');
 
 function forEach(arr, callback) {
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    callback();
+  }
 }
 
 function map(arr, callback) {
-  // Your code here
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(callback(arr[i]));
+  }
+  return newArray;
 }
 
 function filter(arr, callback) {
-  // Your code here
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
 }
 
 function some(arr, callback) {
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i])
 }
 
 function every(arr, callback) {
-  // Your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i])) {
+      return false;
+    }
 }
 
 if (typeof describe === 'function') {
